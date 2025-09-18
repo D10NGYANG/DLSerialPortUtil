1d6e5f4084e232c39daf71803f9d0d4be629784b
D10NGYANG <D10NGYANG@163.com>
2025-09-18 17:17:03 +0800

    feat(AndroidSerialPort): 添加Android串口实现在`AndroidSerialPort`类中实现了Android平台的串口功能，包括打开、读取、写入和关闭串口。主要功能如下：
    - 使用`kotlinx.coroutines`库进行异步操作。
    - 通过`SerialPort`库打开串口，并设置相应的波特率、数据位、校验位和停止位。
    - 提供了修改文件权限的方法`chmod777`，以确保串口文件可读写。
    - 启动一个协程来循环读取串口数据，并将数据通过`outputDataFlow`发送出去。
    - 实现了写入数据到串口的功能。
    - 关闭串口时取消读取任务并释放资源。
    
    此外，还修改了`BaseSerialPort`类中的`outputDataFlow`的缓冲区容量为`Int.MAX_VALUE`，以支持更大的数据流。

---
+++ library/src/androidMain/kotlin/com/d10ng/serialport/AndroidSerialPort.kt
@@ -0,0 +1,103 @@
+package com.d10ng.serialport
+
+import android.serialport.SerialPort
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import java.io.File
+
+/**
+ * Android串口
+ * @Author d10ng
+ * @Date 2025/9/17 13:53
+ */
+class AndroidSerialPort(
+    info: SerialPortInfo,
+    config: SerialPortConfig
+): BaseSerialPort(info, config) {
+
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    private var sp: SerialPort? = null
+
+    // 缓存数据
+    private val buffer = ByteArray(2048)
+    // 循环读取数据任务
+    private var readJob: Job? = null
+
+    override fun open() {
+        if (sp != null) return
+        val file = File(info.id)
+        // 提权
+        if (!file.canRead() || !file.canWrite()) chmod777(file)
+        runCatching {
+            // 打开串口
+            sp = SerialPort(
+                file,
+                config.baudRate.intValue,
+                config.dataBits.intValue,
+                config.parity.intValue,
+                config.stopBits.intValue
+            )
+            startRead()
+            openStateFlow.value = true
+        }.onFailure { exception ->
+            sp = null
+            throw exception
+        }
+    }
+
+    /**
+     * 修改权限
+     * @param device File
+     * @return Boolean
+     */
+    private fun chmod777(device: File): Boolean {
+        if (!device.exists()) return false
+        if (device.canRead() && device.canWrite() && device.canExecute()) return true
+        return runCatching {
+            val su = Runtime.getRuntime().exec("/system/bin/su")
+            val cmd = "chmod 777 ${device.absolutePath}\nexit\n"
+            su.outputStream.write(cmd.toByteArray())
+            0 == su.waitFor() && device.canRead() && device.canWrite() && device.canExecute()
+        }.getOrDefault(false)
+    }
+
+    private fun startRead() {
+        // 启动读取线程
+        readJob = scope.launch {
+            loop@ while (isActive && sp != null) {
+                runCatching {
+                    val size = sp!!.inputStream.read(buffer)
+                    if (size > 0) {
+                        outputDataFlow.tryEmit(buffer.copyOfRange(0, size))
+                    } else if (size == -1) {
+                        break@loop
+                    }
+                }
+            }
+            close()
+        }
+    }
+
+    override fun write(data: ByteArray): Boolean {
+        return runCatching {
+            sp!!.outputStream.use { os ->
+                os.write(data)
+                os.flush()
+            }
+            true
+        }.getOrDefault(false)
+    }
+
+    override fun close() {
+        runCatching { readJob?.cancel() }
+        runCatching { sp?.tryClose() }
+        readJob = null
+        sp = null
+        openStateFlow.value = false
+    }
+}
--- library/src/commonMain/kotlin/com/d10ng/serialport/BaseSerialPort.kt
+++ library/src/commonMain/kotlin/com/d10ng/serialport/BaseSerialPort.kt
@@ -15,7 +15,7 @@ abstract class BaseSerialPort(
     /**
      * 输出数据流
      */
-    val outputDataFlow = MutableSharedFlow<ByteArray>(extraBufferCapacity = 2048)
+    val outputDataFlow = MutableSharedFlow<ByteArray>(extraBufferCapacity = Int.MAX_VALUE)
 
     /**
      * 串口是否处于开启状态
